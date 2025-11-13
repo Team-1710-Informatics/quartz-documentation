109eebb382be44dd45972b9095686d24168f7f94
Sam <finepointdog@gmail.com>
2025-11-13 10:00:57 -0600

    Added the anuppuccin theme.

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Informatics",
-    pageTitleSuffix: "",
+    pageTitle: "Informatics Book",
+    pageTitleSuffix: "Team 1710",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
