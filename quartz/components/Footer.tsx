39b0316a401981fb7fc4c212dbd529951302b993
Sam <bargallosam@gmail.com>
2025-11-13 21:21:09 -0600

    Update footer text format and remove unused links in quartz.layout.ts

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}, Modified by FRC Team 1710
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year} - Modified by FRC Team 1710
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
