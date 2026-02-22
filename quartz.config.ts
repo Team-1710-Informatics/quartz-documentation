10bf08f82b22e9bd8e070646cde35765e1215cd9
Sam <bargallosam@gmail.com>
2026-02-21 18:35:55 -0600

    Update pageTitleSuffix in quartz.config.ts to improve clarity

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Programming Book",
-    pageTitleSuffix: "Team 1710",
+    pageTitleSuffix: " - Team 1710",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
